7facf4c0b84aee1f09294532a6647377b55dfb52
Shakoury <0xbyte01@aeglix.com>
2026-09-04 10:53:26 +0100

    Add Gmail SMTP config for production

--- vtu_backend/config/settings_prod.py
+++ vtu_backend/config/settings_prod.py
@@ -39,3 +39,14 @@ LOGGING['handlers']['file']['filename'] = '/tmp/vtu_platform.log'
 SPECTACULAR_SETTINGS['SERVERS'] = [
     {'url': 'https://api.ibkdata.com', 'description': 'Production Server'},
 ]
+
+
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', '')
+FRONTEND_URL = 'https://ibkdata.com'
