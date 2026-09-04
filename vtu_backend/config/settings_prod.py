1e08c583e6027be789872043fef2388f1cb019a4
Shakoury <0xbyte01@aeglix.com>
2026-09-04 11:00:04 +0100

    Disable SSL redirect for Railway

--- vtu_backend/config/settings_prod.py
+++ vtu_backend/config/settings_prod.py
@@ -27,7 +27,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Security
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
