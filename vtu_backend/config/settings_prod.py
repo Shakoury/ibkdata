1880907bd91523b7bfd01de6c49477c8a7c696b9
Shakoury <0xbyte01@aeglix.com>
2026-09-04 11:35:23 +0100

    Switch to Gmail SSL port 465

--- vtu_backend/config/settings_prod.py
+++ vtu_backend/config/settings_prod.py
@@ -47,7 +47,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '').replace(' ', '')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', os.environ.get('EMAIL_HOST_USER', ''))
 FRONTEND_URL = 'https://ibkdata.com'
