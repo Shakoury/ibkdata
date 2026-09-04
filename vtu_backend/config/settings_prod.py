3ec3194b9d624720a92a4abef562a4a42b28ec1e
Shakoury <0xbyte01@aeglix.com>
2026-09-04 09:22:36 +0100

    Add Railway deployment config

---
+++ vtu_backend/config/settings_prod.py
@@ -0,0 +1,35 @@
+from .settings import *
+import dj_database_url
+import os
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') + ['.railway.app', '.up.railway.app']
+
+# Production database from Railway
+DATABASES = {
+    'default': dj_database_url.config(
+        env='DATABASE_URL',
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+# Production CORS
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
+CORS_ALLOW_ALL_ORIGINS = False
+
+# Static files
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Security
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Logging
+LOGGING['handlers']['file']['filename'] = '/tmp/vtu_platform.log'
